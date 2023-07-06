4f9d6d1491c5e9aa35c4bc709360427b151f875b
ShogoMark <trigmark87@gmail.com>
2023-07-06 18:34:45 +0100

    task2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Function that add two floats and return sum in float"""
+
+import math
+
+def floor(n: float) -> int:
+    """function takes in two floats"""
+
+    return math.floor(n)
