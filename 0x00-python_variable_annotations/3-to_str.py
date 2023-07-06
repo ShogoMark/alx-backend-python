02f63b174649c8ad436c81cc57179d46f4f2499a
ShogoMark <trigmark87@gmail.com>
2023-07-06 18:44:07 +0100

    task3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Function that add two floats and return sum in float"""
+
+import math
+
+
+def to_str(n: float) -> str:
+    """function takes in two floats"""
+
+    return str(n)
