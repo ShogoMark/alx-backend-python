12a2acdc62d049ee8a389d543658de8d874c725e
ShogoMark <trigmark87@gmail.com>
2023-07-06 20:10:49 +0100

    task5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """Function that takes in a list of floats and returns float sum"""
 
+import typing
 
-def sum_list(figs: list[float]) -> float:
+def sum_list(input_list: typing.List[float]) -> float:
     """function takes in two floats"""
 
     sum_float = 0.0
-    for fig in figs:
-        sum_float += fig
+    for lists in input_list:
+        sum_float += lists
 
     return sum_float
