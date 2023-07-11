d108caf177f3d9dd022e088c1c9725d20f7b4129
ShogoMark <trigmark87@gmail.com>
2023-07-11 16:05:55 +0100

    task4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -10,8 +10,8 @@ task_wait_random = __import__('3-tasks').task_wait_random
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """function spawns the delays n number of times"""
     # creating a list of all tasks
-    tasks = [asyncio.create_task(task_wait_random(max_delay))
-             for i in range(n)]
-    delays = await asyncio.gather(*tasks)
+    delays = await asyncio.gather(
+        *tuple(task_wait_random(max_delay) for i in range(n))
+    )
 
     return sorted(delays)
