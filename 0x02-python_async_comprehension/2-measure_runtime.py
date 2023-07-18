928dc9065e85bd2aaaaac954ed19550f9625e692
ShogoMark <trigmark87@gmail.com>
2023-07-18 23:20:57 +0100

    task2

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -6,6 +6,7 @@ import time
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
+
 async def measure_runtime() -> float:
     """
         coroutine that executes async_comprehension 4 times
