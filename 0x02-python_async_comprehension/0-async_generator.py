9c095e59f6b12a3ef8581c2a4d4b3fc36f8766f2
ShogoMark <trigmark87@gmail.com>
2023-07-18 19:44:54 +0100

    task0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""A coroutine called async_generator"""
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+        A coroutine async_generator that yields random numbers
+        between 0 and 10 while asynchronously waiting 1 second 
+    """
+    for n in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
