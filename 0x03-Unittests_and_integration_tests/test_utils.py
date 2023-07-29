2a5717e88dc2c718896da633734f35a2a87c3737
ShogoMark <trigmark87@gmail.com>
2023-07-29 05:00:28 +0100

    task0

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
+"""a TestAccessNestedMap class"""
 
 import unittest
 import pytest
 from parameterized import parameterized
+from typing import Any, Dict, Mapping, Sequence
+
 
 class TestAccessNestedMap(unittest.TestCase):
+    """inherits from unittest.TestCase"""
     @parameterized.expand([
         ({"a": 1}, "a", 1),
         ({"a": {"b": 2}}, "a", KeyError(a)),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_access_nested_map(self, nested_map, path, res):
+    def test_access_nested_map(
+                              self,
+                              nested_map: Mapping,
+                              path: Sequence,
+                              res: Any) -> Any:
+        """test function takes nested_map&path as arguments"""
         result = nested_map[key]
         self.assertEqual(result, res)
