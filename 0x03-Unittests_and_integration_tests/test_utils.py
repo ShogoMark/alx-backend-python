cadd617fd27c1bc5c4e42b3aeb437b715af53de6
ShogoMark <trigmark87@gmail.com>
2023-07-30 06:03:20 +0100

    task1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,9 +14,7 @@ class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
         ({"a": 1}, "a", 1),
         ({"a": {"b": 2}}, "a", KeyError("a")),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
-        ({}, "a", KeyError),
-        ({"a": 1}, "a", "b", KeyError),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(
                               self,
@@ -25,6 +23,10 @@ class TestAccessNestedMap(unittest.TestCase):
                               res: Any) -> Any:
         self.assertEqual(access_nested_map(nested_map, path), res)
 
+    @parameterized.expand([
+        ({}, "a", KeyError),
+        ({"a": 1}, "a", "b", KeyError),
+    ])
     def test_access_nested_map_exception(
                                         self, nested_map: Mapping,
                                         *path_elements: Sequence,
@@ -37,3 +39,4 @@ class TestAccessNestedMap(unittest.TestCase):
                                         else:
                                         self.fail("Expected KeyError
                                                   not raised."))
+    """function to test exceptions"""
